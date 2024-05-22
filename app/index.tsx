aad019ca82bd02f45d1ed134159fa2cb1ca47d45
Wonder Worker <saintnikfreak@gmail.com>
2024-05-22 20:12:13 +0300

    nativewind

---
+++ app/index.tsx
@@ -0,0 +1,62 @@
+// import {
+//   DarkTheme,
+//   DefaultTheme,
+//   ThemeProvider,
+// } from "@react-navigation/native";
+// import { useFonts } from "expo-font";
+// import { Stack } from "expo-router";
+// import * as SplashScreen from "expo-splash-screen";
+// import { useEffect } from "react";
+// import "react-native-reanimated";
+
+import { StatusBar } from "expo-status-bar";
+import { View, Text, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
+// import { useColorScheme } from "@/hooks/useColorScheme";
+
+// // Prevent the splash screen from auto-hiding before asset loading is complete.
+// SplashScreen.preventAutoHideAsync();
+
+// export default function App() {
+//   const colorScheme = useColorScheme();
+//   const [loaded] = useFonts({
+//     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+//   });
+
+//   useEffect(() => {
+//     if (loaded) {
+//       SplashScreen.hideAsync();
+//     }
+//   }, [loaded]);
+
+//   if (!loaded) {
+//     return null;
+//   }
+
+//   return (
+//     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+//       <Stack>
+//         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+//         <Stack.Screen name="+not-found" />
+//       </Stack>
+//     </ThemeProvider>
+//   );
+// }
+
+export default function App() {
+  return (
+    <View>
+      <Text>Wonder Work!</Text>
+      <StatusBar style="auto" />
+      <Link
+        href="/profile"
+        style={{
+          color: "blue",
+        }}
+      >
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
